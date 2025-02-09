21e519aed7086b431042dc545f5275960415dcef
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-09 07:01:34 +0300

    Implement player turns, turns storage

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function createPlayer(name) {
-  const player = name;
-  return player;
+  return { name };
 }
 
-const player1 = createPlayer("Josh");
-const player2 = createPlayer("Nick");
+const gameLogic = (function () {
+  const Gameboard = [];
+  const playerTurn = (player1, player2) => {
+    let current = player1;
+    return function () {
+      current = current === player1 ? player2 : player1;
+      return current;
+    };
+  };
+  const makeMove = (player) => {
+    const move = prompt(`${player.name} turn: `);
+    Gameboard.push({ player: player.name, move });
+  };
+  const logGameboard = () => console.log(Gameboard);
+  return { logGameboard, playerTurn, makeMove };
+})();
+
+const player1 = createPlayer("Knight");
+const player2 = createPlayer("Bruh");
+
+function gameStart() {
+  let current = player1;
+  const toggleTurn = gameLogic.playerTurn(player1, player2);
+  let moveNumber = 1;
+
+  for (let i = 0; i < 4; i++) {
+    gameLogic.makeMove(current);
+    gameLogic.logGameboard();
+    current = toggleTurn();
+    moveNumber++;
+  }
+}
+
+gameStart();
